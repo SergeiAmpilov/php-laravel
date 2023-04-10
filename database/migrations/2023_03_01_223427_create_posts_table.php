851c044ec4ea8dfac9ce2d8839087e931997d26d
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-04-10 10:47:26 +0300

    lesson 10 migrations for columns

--- database/migrations/2023_03_01_223427_create_posts_table.php
+++ database/migrations/2023_03_01_223427_create_posts_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
-            $table->string('slug', 100);
-            $table->text('content');
             $table->timestamps();
+            $table->string('title');
+            $table->text('content');
         });
     }
 
