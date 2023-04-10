45785813083c7107173d55777d8db27267e4619a
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-04-10 12:03:45 +0300

    lesson 11 transactions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,25 +19,38 @@ class HomeController extends Controller
     public function main(): View
     {
 
-        DB::update("UPDATE posts SET created_at = :created_at , updated_at = :updated_at WHERE created_at IS NULL OR updated_at IS NULL", [
-            'created_at' => NOW(),
-            'updated_at' => NOW(),
-        ]);
+        DB::beginTransaction();
+
+        try {
+
+            DB::update("UPDATE posts SET created_at = :created_at  WHERE created_at IS NULL ", [
+                'created_at' => NOW()
+            ]);
+
+            DB::update("UPDATE posts SET updated_at = :updated_at WHERE updated_at IS NULL", [
+                'updated_at' => NOW(),
+            ]);
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            dump($e->getMessage());
+        }
+
 
 //        $query = DB::insert("INSERT INTO posts (title, content) VALUES (?, ?)", ['Hello world', 'This is article 5']);
         /////////
-        $res_db = DB::select("SELECT * FROM posts WHERE id > :id", ['id' => 0]);
-        dump($res_db);
+//        $res_db = DB::select("SELECT * FROM posts WHERE id > :id", ['id' => 0]);
+//        dump($res_db);
         ////////
         ///
 
 
-        return view('welcome');
 
-        /*
+
         $post = new Post();
         $post->title = 'Article 3';
-//        $post->content = 'Post content 3';
+        $post->content = 'Post content 3';
         $post->save();
 
         $data1 = 'data 1';
@@ -45,7 +58,7 @@ class HomeController extends Controller
 
 
         return view('main', compact('data1', 'data2'))->with('var1', $data1 . $data2);
-        */
+
     }
 
     public function test()
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        /*
         DB::listen(function (QueryExecuted $query) {
             dump($query);
         });
+        */
     }
 }
