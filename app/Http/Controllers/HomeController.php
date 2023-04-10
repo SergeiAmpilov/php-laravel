80b89e6aa04c56447ab6e5d4d19de562525d15e4
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-04-10 11:34:25 +0300

    lesson 11 some examples for raw db query

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,6 +19,15 @@ class HomeController extends Controller
     public function main(): View
     {
 
+//        $query = DB::insert("INSERT INTO posts (title, content) VALUES (?, ?)", ['Hello world', 'This is article 5']);
+        /////////
+        $res_db = DB::select("SELECT * FROM posts WHERE id > :id", ['id' => 3]);
+        dump($res_db);
+        ////////
+        ///
+        return view('welcome');
+
+        /*
         $post = new Post();
         $post->title = 'Article 3';
 //        $post->content = 'Post content 3';
@@ -29,6 +38,7 @@ class HomeController extends Controller
 
 
         return view('main', compact('data1', 'data2'))->with('var1', $data1 . $data2);
+        */
     }
 
     public function test()
